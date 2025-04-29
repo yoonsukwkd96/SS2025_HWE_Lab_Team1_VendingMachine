c7fdbca0a6f39a94fb036b2aec257cfd4e871e7a
emaansajid-dev <129278578+emaansajid-dev@users.noreply.github.com>
2025-04-29 11:21:44 +0200

    tb.half_adder.vhd

--- tb.half_adder.vhd
+++ tb.half_adder.vhd
@@ -5,25 +5,23 @@ entity tb_halfadder is
 end tb_halfadder;
 
 architecture sim of tb_halfadder is
-signal A,B,Sum, Carry : STD_LOGIC;
-begin
-component halfadder
-port(
-    A: in STD_LOGIC;
-    B: in STD_LOGIC;
-    Sum: out STD_LOGIC;
-    Carry: out STD_LOGIC
-);
-end component;
+signal A, B, Sum, Carry : STD_LOGIC;
 
-UUT: halfadder port map(A => A, B => B, Sum => Sum, Carry => Carry);
+begin
+UUT: entity work.halfadder port map(
+    A => A, 
+    B => B, 
+    Sum => Sum, 
+    Carry => Carry
+);
+
 process
 begin
-A <= '0'; B <= '0'; wait for 10 ns;
-A <= '0'; B <= '1'; wait for 10 ns;
-A <= '1'; B <= '0'; wait for 10 ns;
-A <= '1'; B <= '1'; wait for 10 ns;
+    A <= '0'; B <= '0'; wait for 10 ns;
+    A <= '0'; B <= '1'; wait for 10 ns;
+    A <= '1'; B <= '0'; wait for 10 ns;
+    A <= '1'; B <= '1'; wait for 10 ns;
 
-wait;
+    wait;
 end process;
 end sim;
