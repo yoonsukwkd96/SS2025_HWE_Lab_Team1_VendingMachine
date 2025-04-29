ec662a6ab78d2b63724b887b03cd4d2c0a4de59b
emaansajid-dev <129278578+emaansajid-dev@users.noreply.github.com>
2025-04-29 12:10:36 +0200

    ripplecarryadder.vhd

--- ripplecarryadder.vhd
+++ ripplecarryadder.vhd
@@ -2,11 +2,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 entity ripple is
 port(
- A0,A1,A2,A3:in  STD_LOGIC;
-  B0,B1,B2,B3:in  STD_LOGIC;
-  Cin:in  STD_LOGIC;
-  Sum0,Sum1,Sum2,Sum3: out STD_LOGIC;
-   Cout: out STD_LOGIC
+ A0,A1,A2,A3:in  bit;
+  B0,B1,B2,B3:in bit;
+  Cin:in  bit;
+  Sum0,Sum1,Sum2,Sum3: out bit;
+   Cout: out bit
 );
 
 
@@ -15,15 +15,15 @@ end ripple;
 architecture Behavioral of ripple is
 component fulladder
 port(
-A:in STD_LOGIC;
-B:in STD_LOGIC;
-Cin:in STD_LOGIC;
-Sum:out STD_LOGIC;
-Cout:out STD_LOGIC
+A:in bit;
+B:in bit;
+Cin:in bit;
+Sum:out bit;
+Cout:out bit
 );
 end component;
 
-signal c1,c2,c3:STD_LOGIC;
+signal c1,c2,c3:bit;
 begin
 FA0:fulladder port map(
 A => A0,
@@ -34,7 +34,7 @@ Cout => c1
 );
 
 
-FA0:fulladder port map(
+FA1:fulladder port map(
 A => A1,
 B => B1,
 Cin => C1,
@@ -42,14 +42,14 @@ Sum => Sum1,
 Cout => c2
 );
 
-FA0:fulladder port map(
+FA2:fulladder port map(
 A => A2,
 B => B2,
 Cin => c2,
 Sum => Sum2,
 Cout => c3
 );
-FA0:fulladder port map(
+FA3:fulladder port map(
 A => A3,
 B => B3,
 Cin => c3,
