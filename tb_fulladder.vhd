a6ec24fdd676a559e23959ece73a3454ad0745bf
emaansajid-dev <129278578+emaansajid-dev@users.noreply.github.com>
2025-04-29 10:12:29 +0200

    tb_fulladder.vhd

--- tb_fulladder.vhd
+++ tb_fulladder.vhd
@@ -1,13 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 entity tb_fulladder is
+
 end tb_fulladder;
 
 architecture test of tb_fulladder is 
-signal A,B, Cin:STD_LOGIC : '0';
+signal A,B, Cin:STD_LOGIC := '0';
 signal Sum,Cout :STD_LOGIC;
 begin
-port map(
+entity work.fulladder port map(
 A=>A,
 B=>B,
 Cin=>Cin,
@@ -27,4 +28,4 @@ A <= '0'; B <= '0'; Cin <= '0'; wait for 10 ns;
         A <= '1'; B <= '1'; Cin <= '1'; wait for 10 ns;
 wait;
 end process;
-end behavior;
+end test;
