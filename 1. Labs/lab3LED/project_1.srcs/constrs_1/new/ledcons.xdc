fd6f7a851c2b294db786986c491de7318b02b5cc
Leano9950 <124461836+Leano9950@users.noreply.github.com>
2025-05-19 20:08:59 +0200

    Add files via upload

---
+++ 1. Labs/lab3LED/project_1.srcs/constrs_1/new/ledcons.xdc
@@ -0,0 +1,2 @@
+set_property PACKAGE_PIN U16 [get_ports LED]
+set_property IOSTANDARD LVCMOS33 [get_ports LED]
---
+++ 1. Labs/lab3LED/project_1.srcs/sources_1/new/turnonleds.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 2025/05/12 18:33:41
+-- Design Name: 
+-- Module Name: turnonleds - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity turnonleds is
+--  Port ( );
+end turnonleds;
+
+architecture Behavioral of turnonleds is
+
+begin
+
+
+end Behavioral;
